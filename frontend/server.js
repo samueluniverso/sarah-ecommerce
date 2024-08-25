90620c58289fc234b1d75083e54fff17ea051d47
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2024-08-25 18:48:54 -0300

    Install SvelteKit

--- frontend/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var app = http.createServer(
-    function(req, res) {
-        res.writeHead(200, {'Content-Type': 'application/json'} );
-        res.end(JSON.stringify( {'message': 'Hello World!'} ));
-    }
-);
-app.listen(3000);
