6472b6fb78d6b22994a34d544be2d4abcdf14601
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2024-08-29 23:54:31 -0300

    User auth routes

--- backend/sarah-api/routes/api.php
+++ backend/sarah-api/routes/api.php
@@ -4,16 +4,14 @@ use App\Http\Controllers\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [
+    UserAuthController::class, 'login'
+]);
+
+Route::post('/logout', [
+    UserAuthController::class, 'logout'
+])->middleware('auth:sanctum');
 
 Route::post('/register', [
     UserAuthController::class, 'register'
 ]);
-Route::post('/login', [
-    UserAuthController::class, 'login'
-]);
-Route::post('/logout', [
-    UserAuthController::class, 'logout'
-])->middleware('auth:sanctum');
