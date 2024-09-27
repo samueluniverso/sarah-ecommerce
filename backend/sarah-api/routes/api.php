25dd6e3c0dcb3da3ecb6f6340c0d9f5a77f91da3
RianWlp <rianwlp@gmail.com>
2024-09-27 20:48:07 -0300

    feat: novas rotas

--- backend/sarah-api/routes/api.php
+++ backend/sarah-api/routes/api.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\PessoaFisicaController;
 use App\Http\Controllers\UserAuthController;
+use App\Http\Controller\CategoriaController;
+use App\Http\Controller\MarcaController;
+use App\Http\Controller\MedidaController;
+use App\Http\Controller\PromocaoController;
 use Illuminate\Support\Facades\Route;
 
 /**
@@ -37,3 +41,101 @@ Route::delete('/pessoa-fisica/{id}', [
 Route::put('/pessoa-fisica/', [
     PessoaFisicaController::class, 'update'
 ])->middleware('auth:sanctum');
+
+
+/**
+ * Categoria
+ */
+Route::get('/categoria/{id}', [
+    CategoriaController::class, 'get'
+])->middleware('auth:sanctum');
+
+Route::post('/categoria/', [
+    CategoriaController::class, 'store'
+])->middleware('auth:sanctum');
+
+Route::delete('/categoria/{id}', [
+    CategoriaController::class, 'delete'
+])->middleware('auth:sanctum');
+
+Route::put('/categoria/', [
+    CategoriaController::class, 'update'
+])->middleware('auth:sanctum');
+
+/**
+ * Marca
+ */
+Route::get('/marca/{id}', [
+    MarcaController::class, 'get'
+])->middleware('auth:sanctum');
+
+Route::post('/marca/', [
+    MarcaController::class, 'store'
+])->middleware('auth:sanctum');
+
+Route::delete('/marca/{id}', [
+    MarcaController::class, 'delete'
+])->middleware('auth:sanctum');
+
+Route::put('/marca/', [
+    MarcaController::class, 'update'
+])->middleware('auth:sanctum');
+
+
+/**
+ * Medida
+ */
+Route::get('/medida/{id}', [
+    MedidaController::class, 'get'
+])->middleware('auth:sanctum');
+
+Route::post('/medida/', [
+    MedidaController::class, 'store'
+])->middleware('auth:sanctum');
+
+Route::delete('/medida/{id}', [
+    MedidaController::class, 'delete'
+])->middleware('auth:sanctum');
+
+Route::put('/medida/', [
+    MedidaController::class, 'update'
+])->middleware('auth:sanctum');
+
+
+/**
+ * Promocao
+ */
+// Route::get('/promocao/{id}', [
+//     Promocao::class, 'get'
+// ])->middleware('auth:sanctum');
+
+// Route::post('/promocao/', [
+//     Promocao::class, 'store'
+// ])->middleware('auth:sanctum');
+
+// Route::delete('/promocao/{id}', [
+//     Promocao::class, 'delete'
+// ])->middleware('auth:sanctum');
+
+// Route::put('/promocao/', [
+//     Promocao::class, 'update'
+// ])->middleware('auth:sanctum');
+
+/**
+ * Produto
+ */
+// Route::get('/produto/{id}', [
+//     Produto:class, 'get'
+// ])->middleware('auth:sanctum');
+
+// Route::post('/produto/', [
+//     Produto:class, 'store'
+// ])->middleware('auth:sanctum');
+
+// Route::delete('/produto/{id}', [
+//     Produto:class, 'delete'
+// ])->middleware('auth:sanctum');
+
+// Route::put('/produto/', [
+//     Produto:class, 'update'
+// ])->middleware('auth:sanctum');
