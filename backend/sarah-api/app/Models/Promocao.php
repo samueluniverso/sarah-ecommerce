ed425ed1a89f12ca46da03c040b6e3557029b929
RianWlp <rianwlp@gmail.com>
2024-09-28 16:13:24 -0300

    feat: criacao da model de Promocoes

--- backend/sarah-api/app/Models/Promocao.php
+++ backend/sarah-api/app/Models/Promocao.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Promocao extends Model
 {
     use HasFactory;
+
+    protected $table = 'promocoes';
+
+    public $incrementing = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'dt_inicio',
+        'dt_fim',
+        'vl_percentual',
+        'vl_absoluto'
+    ];
+
+    // public function produto() : HasOne
+    // {
+    //     return $this->hasOne(Produto::class, 'id', 'fk_produto');
+    // }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [];
+    }
 }
