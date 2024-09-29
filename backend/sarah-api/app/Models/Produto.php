74d97c76e9429edc39dd96c05f94e7af94db30dc
RianWlp <rianwlp@gmail.com>
2024-09-29 16:10:31 -0300

    feat: criacao da model de Produto

---
+++ backend/sarah-api/app/Models/Produto.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'produtos';
+
+    public $incrementing = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nome',
+        'is_destaque',
+        'preco'
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [];
+    }
+}
