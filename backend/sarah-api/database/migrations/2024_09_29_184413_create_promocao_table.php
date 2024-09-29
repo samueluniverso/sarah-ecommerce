447b4412b46d9592c1a849e694dcecc638e86c9a
RianWlp <rianwlp@gmail.com>
2024-09-29 15:52:58 -0300

    feat: nova migration da tabela de promocoes

---
+++ backend/sarah-api/database/migrations/2024_09_29_184413_create_promocao_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promocoes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('dt_inicio');
+            $table->timestamp('dt_fim');
+            $table->float('vl_percentual');
+            $table->float('vl_absoluto');
+            $table->bigInteger('fk_produto')->unsigned()->nullable(false);
+            $table->timestamps();
+        });
+
+        Schema::table('promocoes', function(Blueprint $table) {
+            $table->foreign('fk_produto')->references('id')->on('produtos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promocoes');
+    }
+};
