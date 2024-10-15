4199b7bc6101b96b2674f42233937d8db7900c3e
RianWlp <rianwlp@gmail.com>
2024-10-15 19:59:09 -0300

    fix: comentando metodos abstrados, corrigir depois

--- backend/sarah-api/app/Http/Controllers/Controller.php
+++ backend/sarah-api/app/Http/Controllers/Controller.php
@@ -6,10 +6,11 @@ use Illuminate\Http\Request;
 
 abstract class Controller
 {
-    abstract public function softDelete(Request $request); // PATCH (verbo)
-    abstract public function delete(Request $request);
-    abstract public function store(Request $request);
-    abstract public function update(Request $request);
-    abstract public function get(Request $request);
-    abstract public function listaPaginada(Request $request);
+    // Fixme (Criar uma pasta com interfaces)
+    // abstract public function softDelete(Request $request); // PATCH (verbo)
+    // abstract public function delete(Request $request);
+    // abstract public function store(Request $request);
+    // abstract public function update(Request $request);
+    // abstract public function get(Request $request);
+    // abstract public function listaPaginada(Request $request);
 }
