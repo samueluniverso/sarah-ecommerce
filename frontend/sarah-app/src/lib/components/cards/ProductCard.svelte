7516974bc6c143023d918d1e6bedea1ccc9634fd
Samuel Oberger Rockenbach <samuel.rockenbach@universo.univates.br>
2024-10-19 19:06:17 -0300

    Layout basico para listagem de produtos

---
+++ frontend/sarah-app/src/lib/components/cards/ProductCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import SmallGreenButton from "../form/buttons/SmallGreenButton.svelte";
+    import SmallRedButton from "../form/buttons/SmallRedButton.svelte";
+
+    export let produto: Produto = {id: 0, nome: "", preco: 0.0, quantidade: 0};
+</script>
+
+<div class="card">
+    <header class="card-header">
+        <h2 class="text-4xl font-extrabold dark:text-black">{produto.nome}</h2>
+    </header>
+    <div class="card-body px-4">
+        Quantidade: <strong> {produto.quantidade} </strong>
+    </div>
+    <div class="card-body px-4">
+        Preço: R$ {produto.preco.toFixed(2)}
+    </div>
+    <footer class="card-footer">
+        <SmallGreenButton label="Adicionar +" />
+        <SmallRedButton label="Remover -" />
+    </footer>
+</div>
---
+++ frontend/sarah-app/src/routes/produtos/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import BaseButton from "$lib/components/form/base/BaseButton.svelte";
+    import ProductCard from "$lib/components/cards/ProductCard.svelte";
+
+    const produtos : Produto[] = [
+        {
+            id: 1,
+            nome: "Produto 1",
+            preco: 10.00,
+            quantidade: 4
+        },
+        {
+            id: 2,
+            nome: "Produto 2",
+            preco: 20.00,
+            quantidade: 2
+        },
+        {
+            id: 3,
+            nome: "Produto 3",
+            preco: 30.00,
+            quantidade: 1
+        }
+    ];
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+    <div class="grid grid-cols-3 gap-4">
+        <div class="col-span-3">
+            <h1>Svelte</h1>
+        </div>
+        {#each produtos as produto}
+            <ProductCard produto={produto} />
+        {/each}
+        <div class="col-span-3">
+            <BaseButton label="Carrinho" />
+        </div>
+    </div>
+</div>
