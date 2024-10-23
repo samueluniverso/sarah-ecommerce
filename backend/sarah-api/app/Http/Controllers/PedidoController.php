f2cc12c06429e9ef903e03477a5afcb15154e6da
RianWlp <rianwlp@gmail.com>
2024-10-23 17:26:58 -0300

    feat: criando os controllers

---
+++ backend/sarah-api/app/Http/Controllers/PedidoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PedidoController extends Controller
+{
+    public function store(Request $request) {}
+    public function update(Request $request) {}
+    public function get(Request $request) {}
+
+    // PATCH (verbo)
+    public function softDelete(Request $request) {}
+    public function delete(Request $request) {}
+    public function listaPaginada(Request $request) {}
+}
