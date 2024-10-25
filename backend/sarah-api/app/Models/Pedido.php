5f7149012ba403e2292f21e7a4cb17315b852d24
RianWlp <rianwlp@gmail.com>
2024-10-25 15:41:45 -0300

    feat: criacao das relacoes entre o pedido->pessoa pedido->endereco

--- backend/sarah-api/app/Models/Pedido.php
+++ backend/sarah-api/app/Models/Pedido.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pedido extends Model
@@ -13,4 +14,31 @@ class Pedido extends Model
     protected $table = 'pedidos';
 
     public $incrementing = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [];
+
+    public function pessoa(): HasOne
+    {
+        return $this->hasOne(Pessoa::class, 'id', 'fk_pessoa');
+    }
+
+    public function endereco(): HasOne
+    {
+        return $this->hasOne(Endereco::class, 'id', 'fk_endereco');
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [];
+    }
 }
