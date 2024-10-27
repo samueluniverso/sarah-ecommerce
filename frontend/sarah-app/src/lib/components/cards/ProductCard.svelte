7b99545adb205095ae513ae26dab5eb8c3c98851
Samuel Oberger Rockenbach <samuel.rockenbach@universo.univates.br>
2024-10-27 18:27:49 -0300

    Adicionar produtos (vinddos da API) ao carrinho

--- frontend/sarah-app/src/lib/components/cards/ProductCard.svelte
+++ frontend/sarah-app/src/lib/components/cards/ProductCard.svelte
@@ -3,16 +3,16 @@
     import { itensCarrinho, adicionarAoCarrinho, removerDoCarrinho } from "../../../cart"; 
 
     let carrinho = get(itensCarrinho);
-    let itemCarrinhoIndex = carrinho.findIndex((item) => { return item.id === produto.id; });
+    let itemCarrinhoIndex = carrinho.findIndex((item) => { return item.id === produto.codigo_produto; });
 
     let produtoCarrinho = carrinho[itemCarrinhoIndex];
     itensCarrinho.subscribe((novoItemCarrinho) => {
         carrinho = novoItemCarrinho;
-        itemCarrinhoIndex = carrinho.findIndex((item) => { return item.id === produto.id; });
+        itemCarrinhoIndex = carrinho.findIndex((item) => { return item.id === produto.codigo_produto; });
         produtoCarrinho = carrinho[itemCarrinhoIndex];
     });
 
-    // export let itemCarrinho: ProdutoCarrinho = {id: 0, nome: "", preco: 0.0, quantidade: 0};
+    export let itemCarrinho: ProdutoCarrinho = {id: 0, nome: "", preco: 0.0, quantidade: 0};
     export let produto : any;
 
     import SmallGreenButton from "../form/buttons/SmallGreenButton.svelte";
@@ -24,17 +24,17 @@
     <header class="card-header">
         <h2 class="text-4xl font-extrabold dark:text-black">{produto.produto}</h2>
     </header>
-    <!-- {#if produtoCarrinho !== undefined}
+    <div class="card-body px-1">
+        Preço: R$ {produto.preco}
+    </div>
+    <div><br></div>
+    {#if produtoCarrinho !== undefined}
         <div class="card-body px-4">
             Quantidade: <strong> {produtoCarrinho.quantidade} </strong>
         </div>
     {/if}
-    <div class="card-body px-1">
-        Preço: R$ {produto.preco.toFixed(2)}
-    </div>
-    <div><br></div>
     <footer class="card-footer">
-        <SmallGreenButton onClick={() => adicionarAoCarrinho(produto.id)} label="Adicionar +" />
-        <SmallRedButton onClick={() => removerDoCarrinho(produto.id)} label="Remover -" />
-    </footer> -->
+        <SmallGreenButton onClick={() => adicionarAoCarrinho(produto.codigo_produto)} label="Adicionar +" />
+        <SmallRedButton onClick={() => removerDoCarrinho(produto.codigo_produto)} label="Remover -" />
+    </footer>
 </div>
